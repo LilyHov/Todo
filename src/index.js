314d40163dc7de0d5172ff54c669992f84bbee6b
Lily Hovasapyan <lilyhovasapyan@gmail.com>
2020-07-26 15:10:28 +0400

    added todo list

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+const App = () => {
+    const todoData = [
+        { label: 'Drink Coffee', id: 0},
+        {label : 'Make Awesome App', important: true, id: 1},
+        {label : 'Have a lunch', id: 2}
+    ];
+
+    return (
+        <>
+        <AppHeader/>
+        <SearchPanel/>
+        <TodoList todos={todoData}/>
+        </>
+    )
+};
+
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
